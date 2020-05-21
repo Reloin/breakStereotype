d2116af95391f9c4b6c04cd5f329437b87008ed9
Reloin <yihengleong@gmail.com>
2020-05-21 12:29:37 +0800

    collapse navbar

--- includes/header.php
+++ includes/header.php
@@ -1,19 +1,29 @@
 <?php 
 session_start();
 ?>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 <!-- navbar goes here -->
-<div class="navbar navbar-dark container-fluid">
+<nav class="navbar navbar-expand-lg container-fluid navbar-dark">
     <a class="navbar-brand" href="/">
         <h1 class="title">Break Stereotype</h1>
     </a>
+    <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarMenu">
+    <ul class="navbar-nav ml-auto">
     <?php 
         if(isset($_SESSION['id'])){
-            echo '<a class="nav-btn btn btn-outline-light" href="post.php" role="button">Post</a>';
-            echo '<a class="nav-btn btn btn-outline-dark" href="includes/logout.inc.php" role="button">Logout</a>';
+            echo '<li class="nav-item"><a class="nav-btn btn btn-outline-light" href="post.php" role="button">Post</a></li>';
+            echo '<li class="nav-item"><a class="nav-btn btn btn-outline-dark" href="includes/logout.inc.php" role="button">Logout</a></li>';
         }
         else{
-            echo '<a class="nav-btn btn btn-outline-light" href="login.php" role="button">Post</a>';
+            echo '<li class="nav-item"><a class="nav-btn btn btn-outline-light" href="login.php" role="button">Post</a></li>';
         }
     ?>
-    
-</div>
+    </ul>
+
+    </div>
+</nav>
