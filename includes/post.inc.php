337aea9ab79e2fe3375b6872d6a533a8f78f616c
Reloin <yihengleong@gmail.com>
2020-05-22 16:20:06 +0800

    post for mobile

--- includes/post.inc.php
+++ includes/post.inc.php
@@ -6,16 +6,22 @@ if(isset($_POST["submit-post"])){
     $subject = $_POST["subject"];
     $truth = $_POST["truth"];
 
-    $cmd = "INSERT INTO posts(subject, truth, agree, disagree, author_id, time_posted) VALUES (?, ?, 0, 0, ?, ?)";
-    $stmt = mysqli_stmt_init($conn);
-    if(!mysqli_stmt_prepare($stmt, $cmd)){
-        header("Location: ../post.php?error=sqlerror");
-        exit();
-    }
-    else{
-        mysqli_stmt_bind_param($stmt, "ssis", $subject, $truth, $_SESSION['id'], date('Y-m-d H:i:s'));
-        mysqli_stmt_execute($stmt);
+    if(empty($subject) || empty($truth)){
         header("Location: ../post.php");
         exit();
     }
+    }else{
+        $cmd = "INSERT INTO posts(subject, truth, agree, disagree, author_id, time_posted) VALUES (?, ?, 0, 0, ?, ?)";
+        $stmt = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmt, $cmd)){
+            header("Location: ../post.php?error=sqlerror");
+            exit();
+        }
+        else{
+            mysqli_stmt_bind_param($stmt, "ssis", $subject, $truth, $_SESSION['id'], date('Y-m-d H:i:s'));
+            mysqli_stmt_execute($stmt);
+            header("Location: ../post.php");
+            exit();
+    }
+    
 }
